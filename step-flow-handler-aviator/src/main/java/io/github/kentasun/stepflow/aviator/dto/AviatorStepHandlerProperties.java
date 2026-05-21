aa652808c48aa3aae3e5bd5b24d0c0cfd0df8ffe
孫健太 <45735033+sxd19911008@users.noreply.github.com>
2026-05-21 12:10:13 +0800

    feat: aviator插件包清理lombok

--- step-flow-handler-aviator/src/main/java/io/github/kentasun/stepflow/aviator/dto/AviatorStepHandlerProperties.java
+++ step-flow-handler-aviator/src/main/java/io/github/kentasun/stepflow/aviator/dto/AviatorStepHandlerProperties.java
@@ -1,17 +1,8 @@
 package io.github.kentasun.stepflow.aviator.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * Aviator 表达式引擎配置项
  */
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class AviatorStepHandlerProperties {
 
     /**
@@ -28,4 +19,77 @@ public class AviatorStepHandlerProperties {
      * 是否开启引擎调试日志
      */
     private Boolean traceEval;
+
+    public AviatorStepHandlerProperties(Integer useLRUExpressionCache, Integer maxLoopCount, Boolean traceEval) {
+        this.useLRUExpressionCache = useLRUExpressionCache;
+        this.maxLoopCount = maxLoopCount;
+        this.traceEval = traceEval;
+    }
+
+    public AviatorStepHandlerProperties() {
+    }
+
+    public Integer getUseLRUExpressionCache() {
+        return this.useLRUExpressionCache;
+    }
+
+    public Integer getMaxLoopCount() {
+        return this.maxLoopCount;
+    }
+
+    public Boolean getTraceEval() {
+        return this.traceEval;
+    }
+
+    public void setUseLRUExpressionCache(Integer useLRUExpressionCache) {
+        this.useLRUExpressionCache = useLRUExpressionCache;
+    }
+
+    public void setMaxLoopCount(Integer maxLoopCount) {
+        this.maxLoopCount = maxLoopCount;
+    }
+
+    public void setTraceEval(Boolean traceEval) {
+        this.traceEval = traceEval;
+    }
+
+    public String toString() {
+        return "AviatorStepHandlerProperties(useLRUExpressionCache=" + this.getUseLRUExpressionCache() + ", maxLoopCount=" + this.getMaxLoopCount() + ", traceEval=" + this.getTraceEval() + ")";
+    }
+
+    public static AviatorStepHandlerPropertiesBuilder builder() {
+        return new AviatorStepHandlerPropertiesBuilder();
+    }
+
+    public static class AviatorStepHandlerPropertiesBuilder {
+        private Integer useLRUExpressionCache;
+        private Integer maxLoopCount;
+        private Boolean traceEval;
+
+        AviatorStepHandlerPropertiesBuilder() {
+        }
+
+        public AviatorStepHandlerPropertiesBuilder useLRUExpressionCache(Integer useLRUExpressionCache) {
+            this.useLRUExpressionCache = useLRUExpressionCache;
+            return this;
+        }
+
+        public AviatorStepHandlerPropertiesBuilder maxLoopCount(Integer maxLoopCount) {
+            this.maxLoopCount = maxLoopCount;
+            return this;
+        }
+
+        public AviatorStepHandlerPropertiesBuilder traceEval(Boolean traceEval) {
+            this.traceEval = traceEval;
+            return this;
+        }
+
+        public AviatorStepHandlerProperties build() {
+            return new AviatorStepHandlerProperties(this.useLRUExpressionCache, this.maxLoopCount, this.traceEval);
+        }
+
+        public String toString() {
+            return "AviatorStepHandlerProperties.AviatorStepHandlerPropertiesBuilder(useLRUExpressionCache=" + this.useLRUExpressionCache + ", maxLoopCount=" + this.maxLoopCount + ", traceEval=" + this.traceEval + ")";
+        }
+    }
 }
